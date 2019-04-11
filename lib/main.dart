38d2bb609c7f28bcfd9f8e5e50cb9b82fe334d28
sherlockbeard <b417044@iiit-bh.ac.in>
2019-04-11 13:08:26 +0530

    added a descrpition and close button

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ void main() => runApp(new TodoApp());
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Todo List',
-      home: new TodoList()
-    );
+    return new MaterialApp(title: 'Todo List', home: new TodoList());
   }
 }
 
@@ -22,99 +19,96 @@ class TodoListState extends State<TodoList> {
 
   // This will be called each time the + button is pressed
   void _addTodoItem(String task) {
-  // Only add the task if the user actually entered something
-  if(task.length > 0) {
-    setState(() => _todoItems.add(task));
+    // Only add the task if the user actually entered something
+    if (task.length > 0) {
+      setState(() => _todoItems.add(task));
+    }
   }
-}
 
   // Build the whole list of todo items
   Widget _buildTodoList() {
     return new ListView.builder(
       itemBuilder: (context, index) {
-        // itemBuilder will be automatically be called as many times as it takes for the
-        // list to fill up its available space, which is most likely more than the
-        // number of todo items we have. So, we need to check the index is OK.
-        if(index < _todoItems.length) {
-          return _buildTodoItem(_todoItems[index]);
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
         }
       },
     );
   }
 
-  // Build a single todo item
-  Widget _buildTodoItem(String todoText) {
+  Widget _buildTodoItem(String todoText, int index) {
     return new ListTile(
-      title: new Text(todoText)
-    );
+        title: new Text(todoText), onTap: () => _promptRemoveTodoItem(index));
   }
 
   @override
   Widget build(BuildContext context) {
-  return new Scaffold(
-    appBar: new AppBar(
-      title: new Text('Todo List')
-    ),
-    body: _buildTodoList(),
-    floatingActionButton: new FloatingActionButton(
-      onPressed: _pushAddTodoScreen, // pressing this button now opens the new screen
-      tooltip: 'Add task',
-      child: new Icon(Icons.add)
-    ),
-  );
-}
-void _pushAddTodoScreen() {
-  // Push this page onto the stack
-  Navigator.of(context).push(
-    // MaterialPageRoute will automatically animate the screen entry, as well
-    // as adding a back button to close it
-    new MaterialPageRoute(
-      builder: (context) {
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Add a new task')
-          ),
-          body: new TextField(
-            autofocus: true,
-            onSubmitted: (val) {
-              _addTodoItem(val);
-              Navigator.pop(context); // Close the add todo screen
-            },
-            decoration: new InputDecoration(
-              hintText: 'Enter something to do...',
-              contentPadding: const EdgeInsets.all(16.0)
-            ),
-          )
-        );
-      }
-    )
-  );
-}
-void _removeTodoItem(int index) {
-  setState(() => _todoItems.removeAt(index));
-}
-void _promptRemoveTodoItem(int index) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return new AlertDialog(
-        title: new Text('Mark "${_todoItems[index]}" as done?'),
-        actions: <Widget>[
-          new FlatButton(
-            child: new Text('CANCEL'),
-            onPressed: () => Navigator.of(context).pop()
-          ),
-          new FlatButton(
-            child: new Text('MARK AS DONE'),
-            onPressed: () {
-              _removeTodoItem(index);
-              Navigator.of(context).pop();
-            }
-          )
-        ]
-      );
-    }
-  );
-}
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Todo List')),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed:
+              _pushAddTodoScreen, // pressing this button now opens the new screen
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
+    );
+  }
 
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        new MaterialPageRoute(builder: (context) {
+          String val1="";
+          String val2="";
+      return new Scaffold(
+          appBar: new AppBar(title: new Text('Add a new task')),
+          body: Column(
+            children: <Widget>[
+              new TextField(
+                autofocus: true,
+                onChanged: (text) {
+                  val1 = text; 
+                },
+                decoration: new InputDecoration(
+                    hintText: 'Enter something to do...',
+                    contentPadding: const EdgeInsets.all(16.0)),
+              ),
+              TextField(
+                onChanged: (text){
+                  val2 = text;
+                },
+                decoration: InputDecoration(
+                    hintText: 'Enter the despricption',
+                    contentPadding: EdgeInsets.all(16)),
+              ), RaisedButton(onPressed:(){ _addTodoItem(val1);Navigator.pop(context);},child: Text('i guess'),)
+            ],
+          ));
+    }));
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Mark "${_todoItems[index]}" as done?'),
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()),
+                new FlatButton(
+                    child: new Text('MARK AS DONE'),
+                    onPressed: () {
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    })
+              ]);
+        });
+  }
 }
